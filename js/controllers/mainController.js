9c5b8ae5741aef040137e39e05ef5009dce81caf
medaak <medaakdev@gmail.com>
2016-11-06 22:21:22 +0100

    Fin de la quête

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,20 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+
+  $scope.addPerson = function(person) {
+  $scope.persons.push(person);
+  $scope.person={};
+  };
+  
+    $scope.persons = [{
+      lastName : "Fontaine",
+      firstName : "Jean",
+      email : "jeandlafont@gmail.com"
+    },
+    {
+      lastName : "Truite",
+      firstName : "Jeanne",
+      email : "jeannedlatruite@gmail.com"
+    }
+  ];
 }
